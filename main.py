389e92d17a18489fa747c6f67b0535ae01b45609
JMNofziger <jacob.nofziger@gmail.com>
2017-07-19 01:23:01 -0700

    corrected naming of body column, added needed routes and todo items

--- main.py
+++ main.py
@@ -15,19 +15,31 @@ db = SQLAlchemy(app)
 class Post(db.Model):
     id=db.Column(db.Integer, primary_key = True)
     title=db.Column(db.String(120))
-    content=db.Column(db.Text)
+    body=db.Column(db.Text)
     date=db.Column(db.DateTime)
     draft=db.Column(db.Boolean, default=True)
 
-    def __init__(self, title, content, draft):
+    def __init__(self, title, body, draft):
         self.title=title
-        self.content=content
+        self.body=body
         self.draft=draft
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return "<h1>Shalom</h1>"
+    return render_template("base.html", title="JMN BAB Blog")
+
+@app.route('/newpost', methods=['POST'])
+def new_post():
+    #TODO: After submitting a new post, your app displays the main blog page.
+    #TODO: You're able to submit a new post.
+    #TODO: If title or body is left empty the form is rendered again, with a helpful error message and any previously-entered content preserved
+    return render_template("new-post.html")
+
+@app.route('/blog', methods=['GET'])
+def get_posts():
+    #TODO: displays all the blog posts
+    return render_template('all-posts.html')
 
 if __name__ == '__main__':
     app.run()
