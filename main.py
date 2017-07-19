0f5fd6763a82e2f3615644a6b2a21af64723f6b3
JMNofziger <jacob.nofziger@gmail.com>
2017-07-19 03:14:10 -0700

    Completed initial handling of new posts and their addition to db, added view of posts in the blog homepage template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import configparser 
 
 app = Flask(__name__)
@@ -16,25 +17,36 @@ class Post(db.Model):
     id=db.Column(db.Integer, primary_key = True)
     title=db.Column(db.String(120))
     body=db.Column(db.Text)
-    date=db.Column(db.DateTime)
-    draft=db.Column(db.Boolean, default=True)
+    date=db.Column(db.DateTime, nullable=False)
+    draft=db.Column(db.Boolean, default=True, nullable=False)
 
     def __init__(self, title, body, draft):
         self.title=title
         self.body=body
         self.draft=draft
-
+        self.date=datetime.now()
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("blog.html", title="JMN BAB Blog")
+    posts = Post.query.all()
+    return render_template("blog.html",posts=posts, title="JMN BAB Blog")
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
     #TODO: After submitting a new post, your app displays the main blog page.
     #TODO: You're able to submit a new post.
     #TODO: If title or body is left empty the form is rendered again, with a helpful error message and any previously-entered content preserved
-    return render_template("new-post.html", errors=errors)
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_body = request.form['body']
+        post_draft = request.form.get('draft')
+        new_post=Post(post_title,post_body,post_draft)
+        db.session.add(new_post)
+        db.session.commit()
+    
+    return render_template("new-post.html")
+
+#@app.route('/single', methods=['POST','GET'])
 
 @app.route('/blog', methods=['GET'])
 def get_posts():
